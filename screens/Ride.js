c26816a5b7dfe0ac21329b54169dc239d86adbb7
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-17 19:17:39 -0600

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -5,7 +5,7 @@ export default class RideScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Ride Screen</Text>
+        <Text style={styles.text}>Pantalla de viajes</Text>
       </View>
     );
   }
